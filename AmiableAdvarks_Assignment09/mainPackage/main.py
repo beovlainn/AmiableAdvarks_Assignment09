54541e1505acd085922ef0973efbf9401f94a74f
bolinen <bolinen@LCB-21>
2025-04-01 16:36:50 -0400

    Add project files.

---
+++ AmiableAdvarks_Assignment09/mainPackage/main.py
@@ -0,0 +1,12 @@
+# main.py
+# Student Name: {required}
+# email:  {required}
+# Assignment Number: Assignment 09
+# Due Date:   4/3/2025
+# Course #/Section:   IS4010-001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:   
+# Brief Description of what this module does: 
+
+# Citations: N/A
+
