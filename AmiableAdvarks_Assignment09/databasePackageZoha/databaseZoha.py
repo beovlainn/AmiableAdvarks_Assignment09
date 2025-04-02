76840f2f35b54823df1f727052c872800b822690
Zoha Iqbal <iqbalza@mail.uc.edu>
2025-04-02 19:14:32 -0400

    I did the 2nd step of the assignment

--- AmiableAdvarks_Assignment09/databasePackageZoha/databaseZoha.py
+++ AmiableAdvarks_Assignment09/databasePackageZoha/databaseZoha.py
@@ -1,11 +1,20 @@
-# file name: 
-# Student Name: {required}
-# email:  {required}
+# file name: databaseZoha
+# Student Name: Zoha Iqbal
+# email: iqbalza@mail.uc.edu
 # Assignment Number: Assignment 09
 # Due Date:   4/3/2025
 # Course #/Section:   IS4010-001
 # Semester/Year:   Spring 2025
-# Brief Description of the assignment:   
-# Brief Description of what this module does: 
+# Brief Description of the assignment: This assignment is supposed to create a sentence  
+# Brief Description of what this module does: "This module retrieves product data from a database and selects a random product
 
 # Citations: N/A
+
+import random
+
+def fetch_products(cursor):
+    cursor.execute("SELECT ProductID, [UPC-A], Description, ManufacturerID, BrandID FROM tProduct")
+    return cursor.fetchall()
+
+def select_random_product(products):
+    return random.choice(products)
