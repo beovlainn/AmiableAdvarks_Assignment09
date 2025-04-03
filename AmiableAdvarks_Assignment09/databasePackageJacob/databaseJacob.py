7bbc2be6c021e07183e9fee364111e354e0b89b8
Jacob Farrell <farrelcj@mail.uc.edu>
2025-04-02 20:06:20 -0400

    Added code to main.py and databaseJacob.py

--- AmiableAdvarks_Assignment09/databasePackageJacob/databaseJacob.py
+++ AmiableAdvarks_Assignment09/databasePackageJacob/databaseJacob.py
@@ -9,3 +9,14 @@
 # Brief Description of what this module does: 
 
 # Citations: N/A
+
+import pyodbc
+
+def get_connection():
+    return pyodbc.connect(
+        'Driver={SQL Server};'
+        'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+        'Database=GroceryStoreSimulator;'
+        'uid=IS4010Login;'
+        'pwd=P@ssword2;'
+    )
